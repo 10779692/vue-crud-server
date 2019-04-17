cd70bae96a1846238b91267437513549fe0e6da9
Jared Reed <jaredreed@Jareds-MacBook-Air.local>
2019-04-16 22:19:00 -0600

    commiting again

--- product.routes.js
+++ product.routes.js
@@ -1,20 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const products = ('../product.controller.js');
+const products = "./product.controller.js";
 
-    // Create a new Product
-    router.post('/products', products.create);
+// Create a new Product
+router.post("/products", products.create);
 
-    // Retrieve all Products
-    router.get('/products', products.findAll);
+// Retrieve all Products
+router.get("/products", products.findAll);
 
-    // Retrieve a single Product with productId
-    router.get('/products/:productId', products.findOne);
+// Retrieve a single Product with productId
+router.get("/products/:productId", products.findOne);
 
-    // Update a Note with productId
-    router.put('/products/:productId', products.update);
+// Update a Note with productId
+router.put("/products/:productId", products.update);
 
-    // Delete a Note with productId
-    router.delete('/products/delete/:productId', products.delete);
+// Delete a Note with productId
+router.delete("/products/delete/:productId", products.delete);
 
-    module.exports = router;
+module.exports = router;
