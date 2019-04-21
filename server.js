f0dfa3fc8f39e3bd2891bc8709af00ea02ca4e0f
Jared Reed <jaredreed@Jareds-MacBook-Air.local>
2019-04-21 17:14:54 -0600

    try

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 
 //Enable CORS for all HTTP methods
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "request.header.origin");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
